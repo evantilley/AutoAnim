ebaf06cd3185731a7b4a88d47eb3c6806217f973
evantilley <elt2141@clac.cs.columbia.edu>
2020-02-27 00:14:13 -0500

    first commit

---
+++ v2/app.js
@@ -0,0 +1,75 @@
+let express = require('express')
+let multer = require('multer')
+let ejs = require('ejs')
+let path = require('path')
+
+
+//set storage engine
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+});
+
+let upload = multer({
+    storage: storage,
+    //10MB limit on filesize
+    limits: {fileSize: 10000000},
+    fileFilter: function(req, file, cb){
+        checkFileType(file, cb);
+    }
+}).single('myImage')
+
+//check file type function
+function checkFileType(file, cb){
+    //allowed extensions
+    let filetypes = /jpeg|jpg|png|gif/
+    //check extension
+    let extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    //check mime
+    let mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname){
+        return cb(null, true)
+    } else{
+        cb('Error: Images Only')
+    }
+
+}
+
+//init app
+let app = express()
+
+//ejs
+app.set('view engine', 'ejs')
+
+app.use(express.static('./public'))
+
+let port = 3000;
+
+app.get('/', (req, res) => res.render('index'))
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err){
+            //if error re-render template with error
+            res.render('index', {
+                msg: err
+            })
+        } else{
+            if(req.file == undefined){
+                res.render('index', {
+                    msg: 'Error: No File Selected'
+                })
+            } else{
+                res.render('index', {
+                    msg: 'File uploaded',
+                    file: `uploads/${req.file.filename}`
+                })
+            }
+        }
+    })
+})
+
+app.listen(port, () => console.log("Server started on port 3000"))
---
+++ v2/public/javascript/script.js
@@ -0,0 +1,127 @@
+function rotate(){
+    anime({
+        targets: '#wheel',
+          rotate: '1turn',
+         loop:true,
+         easing: 'linear',
+        duration: 21500,
+      });
+}
+function rotate1(){
+    anime({
+        targets: '#ferris',
+          rotate: '1turn',
+         loop:true,
+         easing: 'linear',
+        duration: speed,
+      });
+}
+window.setInterval(function(){
+    console.log($("#sliderRange")[0]['value'])
+    speed = $("#sliderRange")[0]['value']
+    $("#current_speed").html("Current speed is " + speed)
+    rotate1()
+    /// call your function here
+  }, 500);
+
+
+var speed = 21500
+
+function resize(){
+    var img = document.querySelector("#ferris");
+    var currWidth = img.clientWidth;
+    var currHeight = img.clientHeight;
+    var userImage = document.querySelector("#user-image");
+    var customWidth = userImage.clientWidth;
+    var customHeight = userImage.clientHeight;
+    userImage.style.width = (currWidth) + "px";
+    userImage.style.height = (currHeight) + "px"
+   // console.log($("#user-image"))
+    //$(".image3").attr("src", $(this)[0]['src'])
+}
+
+
+$(document).ready(function(){
+    rotate()
+    rotate1()
+    resize()
+
+    $("#current_speed").html("Current speed is: " + speed)
+        
+
+    $("#tire").click(function(){
+        console.log("asdfsad")
+        var img = document.querySelector("#ferris");
+        var currWidth = img.clientWidth;
+        var currHeight = img.clientHeight;
+        console.log("width of ferris wheel" + currWidth)
+        console.log("height of ferris wheel" + currHeight)
+        var userImage = document.querySelector("#tire");
+        var customWidth = userImage.clientWidth;
+        var customHeight = userImage.clientHeight;
+        img.style.width = (currWidth) + "px";
+        img.style.height = (currHeight) + "px"
+        $(".image3").attr("src", "/images/tires.png")
+        console.log("width of custom image" + customWidth)
+        console.log("height of current image" + customHeight)
+    })
+
+    $("#susling").click(function(){
+        console.log("asdfgasdfas")
+    })
+    $("#ferris_wheel_select").click(function(){
+        var img = document.querySelector("#ferris");
+        var currWidth = img.clientWidth;
+        var currHeight = img.clientHeight;
+        var userImage = document.querySelector("#tire");
+        var customWidth = userImage.clientWidth;
+        var customHeight = userImage.clientHeight;
+        img.style.width = (currWidth) + "px";
+        img.style.height = (currHeight) + "px"
+        $(".image3").attr("src", "/images/ferris_wheel.png")
+        console.log("width of custom image" + customWidth)
+        console.log("height of current image" + customHeight)
+    })
+
+    $("#user-image").click(function(){
+        var img = document.querySelector("#ferris");
+        var currWidth = img.clientWidth;
+        var currHeight = img.clientHeight;
+        var userImage = document.querySelector("#tire");
+        var customWidth = userImage.clientWidth;
+        var customHeight = userImage.clientHeight;
+        img.style.width = (currWidth) + "px";
+        img.style.height = (currHeight) + "px"
+        $(".image3").attr("src", $(this)[0]['src'])
+    })
+
+    $("#lemon").click(function(){
+        var img = document.querySelector("#ferris");
+        var currWidth = img.clientWidth;
+        var currHeight = img.clientHeight;
+        var userImage = document.querySelector("#tire");
+        var customWidth = userImage.clientWidth;
+        var customHeight = userImage.clientHeight;
+        img.style.width = (currWidth) + "px";
+        img.style.height = (currHeight) + "px"
+        $(".image3").attr("src", "/images/lemon.png")
+        console.log("width of custom image" + customWidth)
+        console.log("height of current image" + customHeight)
+    })
+    $("#columbia").click(function(){
+        var img = document.querySelector("#ferris");
+        var currWidth = img.clientWidth;
+        var currHeight = img.clientHeight;
+        var userImage = document.querySelector("#tire");
+        var customWidth = userImage.clientWidth;
+        var customHeight = userImage.clientHeight;
+        img.style.width = (currWidth) + "px";
+        img.style.height = (currHeight) + "px"
+        $(".image3").attr("src", "/images/columbia_logo_circle.png")
+        console.log("width of custom image" + customWidth)
+        console.log("height of current image" + customHeight)
+    })
+
+})   
+   
+   
